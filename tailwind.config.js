fcc857e858c58b5b124fce53fbc6573e281e9a12
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2024-06-27 16:45:18 +0600

    modified tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}", // Adjust paths based on your folder structure
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        customColor: "#A9C2C9",
-        buttonCustomColor: "#ED2127",
-        textColor: "#B9B9B9",
-        gradColor: "#50505080",
-        circleColor: "#F3F5F6",
-        footerColor: "#181E20",
-        emailColor: "#2D373A",
-        bodyColor: "#212525",
-        white60: "rgba(255, 255, 255, 0.6)",
-      },
+    colors: {
+      customColor: "#A9C2C9",
+      buttonCustomColor: "#ED2127",
+      textColor: "#B9B9B9",
+      gradColor: "#50505080",
+      circleColor: "#F3F5F6",
+      footerColor: "#181E20",
+      emailColor: "#2D373A",
+      bodyColor: "#212525",
+      white60: "rgba(255, 255, 255, 0.6)",
+    },
 
-      fontFamily: {
-        roboto: ["Roboto"],
-        helvetica: ["Helvetica"],
-      },
+    fontFamily: {
+      roboto: ["Roboto"],
+      helvetica: ["Helvetica"],
     },
   },
-
   plugins: [],
 };
