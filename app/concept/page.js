e79edebc5edc36ad04cf6b749f9490a9e0e0fc54
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2024-06-28 23:19:59 +0600

    added gallery and concept page

---
+++ app/concept/page.js
@@ -0,0 +1,15 @@
+import Footer from "@/src/components/common/Footer";
+import Header from "@/src/components/common/Header";
+import Concept from "@/src/components/Concept";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Concept />
+      <Footer />
+    </>
+  );
+};
+
+export default page;
