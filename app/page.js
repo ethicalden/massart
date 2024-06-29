b53caf6fa9508158d09425bb9351eeba69ec78cc
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2024-06-29 20:17:16 +0600

    gallery page modified

--- app/page.js
+++ app/page.js
@@ -2,14 +2,12 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/order */
 /* eslint-disable prettier/prettier */
-import Concept from "@/src/components/Concept";
 import AskQuestion from "@/src/components/FreequentlyAskQuestion/AskQuestion";
 
 import Explore from "@/src/components/Explore";
 import Gallery from "@/src/components/Gallery";
 import MasterClass from "@/src/components/MasterClass";
 import Review from "@/src/components/Review";
-import Video from "@/src/components/Video";
 
 import HomeAboutUs from "@/src/components/HomeAboutUs";
 import Visual from "@/src/components/Visuals";
@@ -22,13 +20,8 @@ export default function Home() {
       <Header />
       <main>
         <HomeAboutUs />
-
         <Visual />
-
         <Gallery />
-
-        <Concept />
-        <Video />
         <Explore />
         <MasterClass />
         <Review />
