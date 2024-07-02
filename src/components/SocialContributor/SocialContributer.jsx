80593bc36bd25c97ac37cb22bebe9b9ac6e3aa30
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2024-07-02 23:56:11 +0600

    fixed

--- src/components/SocialContributor/SocialContributer.jsx
+++ src/components/SocialContributor/SocialContributer.jsx
@@ -10,29 +10,38 @@ const SocialContributor = () => {
         className="grid grid-cols-3 xl:mx-[62px] xl:gap-x-[59px] lg:mx-[50px]  lg:gap-x-[50px] 
       sm:mx-[30px]  sm:gap-x-[35px]  xs:mx-[25px]  xs:gap-x-[25px] "
       >
-        <Image
+        <div>
+         <Image
           className="xl:h-[98px] lg:h-[98px] md:h-[98px] sm:h-[98px] xs:h-[98px]  xl:w-full md:w-full sm:w-full xs:w-full"
           src="/images/BritishCouncil_Logo_Indigo_RGB 2 (1).png"
           alt="logo"
            width={100}
            height={100}
         />
+        </div>
+       
 
+      <div className="w-100 h-50 flex items-center justify-center">
         <Image
-          className="xl:h-[98px] lg:h-[98px] md:h-[98px] sm:h-[98px] xs:h-[98px]  xl:w-full md:w-full sm:w-full xs:w-full  bg-[#EFEDED]"
-          src="/images/pngwing.com (1) 1.png"
-          alt="logo"
-           width={100}
-           height={100}
-        />
+                className="xl:w-full md:w-full sm:w-full xs:w-full  bg-[#EFEDED]"
+                src="/images/pngwing.com (1) 1.png"
+                alt="logo"
+                width={100}
+                height={100}
+              />
+      </div>
+      
 
-        <Image
-          className="xl:h-[98px] lg:h-[98px] md:h-[98px] sm:h-[98px] xs:h-[98px] xl:w-full md:w-full sm:w-full xs:w-full "
-          src="/images/India UK Season of Culture - Third Party Badge_RGB 1.png"
-          alt="logo"
-           width={100}
-           height={100}
-        />
+        <div>
+            <Image
+                  className="xl:h-[98px] lg:h-[98px] md:h-[98px] sm:h-[98px] xs:h-[98px] xl:w-full md:w-full sm:w-full xs:w-full "
+                  src="/images/India UK Season of Culture - Third Party Badge_RGB 1.png"
+                  alt="logo"
+                  width={100}
+                  height={100}
+                />
+        </div>
+      
       </div>
       <hr className="xl:my-[39px] bg-black" />
       <div className="grid grid-cols-3  xl:mx-[62px] xl:gap-x-[59px]">
