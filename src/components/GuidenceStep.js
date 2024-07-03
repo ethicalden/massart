5dcf19a19345d1cc04b37e062f4110957001b1b0
Mou <mahejabinmou.99@gmail.com>
2024-07-02 22:22:26 -0700

    Guidence step responsive

--- src/components/GuidenceStep.js
+++ src/components/GuidenceStep.js
@@ -7,39 +7,45 @@ const AccordionItem = ({ number, title, content, isOpen, onClick }) => (
   <div className="rounded-lg overflow-hidden mb-4">
     <button
       onClick={onClick}
-      className={`w-full text-left py-4 font-bold flex items-center ${
-        isOpen ? "bg-buttonCustomColor text-white" : "bg-white"
-      } transition-colors duration-300`}
+      className={`w-full text-left  font-bold flex 
+       xl:px-[32px] lg:px-[25px]
+       md:px-[20px] sm:px-[15px] xs:px-[15px] ${
+         isOpen ? "bg-buttonCustomColor text-white" : "bg-white"
+       } transition-colors duration-300`}
     >
-      <div className="flex w-full">
-        <div className="w-1/6 flex items-center justify-center">
-          <span className="font-bold lg:text-[32px] md:text-[25px] sm:text-[20px] ">
+      <div className="flex justify-between place-items-baseline">
+        <div className="w-1/6  ">
+          <span className="font-bold lg:text-[32px] md:text-[25px] sm:text-[20px]  xs:text-[18px] ">
             {number}
           </span>
         </div>
-        <div className="w-5/6 flex lg:text-[32px] md:text-[25px] sm:text-[20px] font-bold justify-between items-center">
-          <span className="font-helvetica">{title}</span>
-          {isOpen ? (
-            <HiMiniMinusSmall className="text-buttonCustomColor bg-white h-10 w-10 mr-2 flex items-center justify-center rounded-full" />
-          ) : (
-            <HiMiniPlusSmall className="text-black bg-circleColor h-10 w-10 flex items-center justify-center rounded-full" />
-          )}
+        <div className="w-5/6 ">
+          <div className=" flex  font-bold justify-between items-center">
+            <span className="font-helvetica lg:text-[32px] md:text-[25px] sm:text-[20px]  xs:text-[16px]">
+              {title}
+            </span>
+            {isOpen ? (
+              <HiMiniMinusSmall className="text-buttonCustomColor bg-white xl:w-[28px] xl:h-[28px] lg:w-[28px] lg:h-[28px] md:w-[28px] md:h-[28px]  xs:w-[28px] xs:h-[28px] sm:w-[28px] sm:h-[20px] flex items-center justify-center rounded-[24px]" />
+            ) : (
+              <HiMiniPlusSmall className="text-black bg-circleColor xl:w-[28px] xl:h-[28px] lg:w-[28px] lg:h-[28px] md:w-[28px] md:h-[28px]  xs:w-[28px] xs:h-[28px] sm:w-[28px] sm:h-[20px]  flex items-center justify-center  rounded-[24px]" />
+            )}
+          </div>
+          <div
+            className={`transition-max-height duration-300 ease-in-out overflow-hidden ${
+              isOpen ? "max-h-90" : "max-h-0"
+            }`}
+          >
+            <div
+              className={` md:pb-[30px] md:pt-[30px] lg:pb-[30px] lg:pt-[30px] xl:pb-[40px] xl:pt-[40px]  sm:pb-[25px] sm:pt-[25px]   xs:pb-[25px] xs:pt-[25px]  lg:text-[15px] md:text-[15px] sm:text-[12px]  xs:text-[12px] font-normal ${
+                isOpen ? "bg-buttonCustomColor" : "bg-white"
+              } text-white`}
+            >
+              {content}
+            </div>
+          </div>
         </div>
       </div>
     </button>
-    <div
-      className={`transition-max-height duration-300 ease-in-out overflow-hidden ${
-        isOpen ? "max-h-90" : "max-h-0"
-      }`}
-    >
-      <div
-        className={`p-4 md:px-[90px] md:pb-[40px]   lg:text-[15px] md:text-[15px] sm:text-[12px] font-normal ${
-          isOpen ? "bg-buttonCustomColor" : "bg-white"
-        } text-white`}
-      >
-        {content}
-      </div>
-    </div>
   </div>
 );
 
@@ -52,32 +58,33 @@ const GuidenceStep = () => {
 
   const items = [
     {
-      title: "How do I purchase tickets for an event?",
+      title: "what is durga puja Art ?",
       content:
-        "To purchase tickets, simply log in to your account, select the desired event, and follow the purchasing guide. Alternatively, you can purchase tickets without having to create an account first.",
+        "Durga puja is bengal’s biggest festival that takes place over a week or so in september-october, on dates varying every year but announced well in advance. at the centre of the celebration is the figure of goddess durga, crafted traditionally in clay, who is worshipped in the act of vanquishing the demon mahishasura. it is particularly in the streets of kolkata (calcutta), the capital of the state, that durga puja transcends religious bounds and serves as the cradle for the world’s largest publicly funded Art show. the idols and structures created from months in advance in street corners – become exquisite works of artistic expression, unique to each locality every year. thus there is a different artistic interpretation offered in every locality through the choice of a different theme to which the entire structure conforms. every autumn, the entire city wakes up to an explosion of creativity, with every neighbourhood decked up in unique templates that are a mélange of traditional Art and contemporary concepts, that are diverse and creative enough to rival any international Art biennale. ",
     },
     {
-      title: "How can I cancel or change my ticket order?",
+      title: "What is massArt ?",
       content:
         "To cancel or change your ticket order, please contact our support team.",
     },
     {
-      title: "Are there other payment options besides credit card?",
+      title:
+        "How did suddenly kolkata durga puja Art come in the national and international limelight ?",
       content:
         "Yes, we also accept PayPal and other electronic payment methods.",
     },
     {
-      title: "Can tickets that have been purchased be refunded?",
+      title: "Why durga puja Art is the world’s biggest public Art festival ?",
       content:
         "Refund policies vary depending on the event. Please refer to the event's specific refund policy for details.",
     },
     {
-      title: "How do I get more information about a specific event?",
+      title: "What is the main objective behind psdpa ?",
       content:
         "You can find more information about specific events on our events page or by contacting our support team.",
     },
     {
-      title: "What should I do if I lose my ticket?",
+      title: "What is the main objective behind psdpa ?",
       content:
         "If you lose your ticket, please contact our support team to reissue your ticket.",
     },
